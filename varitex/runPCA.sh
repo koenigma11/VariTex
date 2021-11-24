3ea6b1a2086dd2376dc20584d26685881a8578b9
koenigma <koenigma@student.ethz.ch>
2021-11-24 09:50:51 +0100

    minor change

--- varitex/runPCA.sh
+++ varitex/runPCA.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-bsub -W 24:00 -n 8 -R "rusage[mem=6144]" -o pca_B5000_NAll python glo_pca.py --n_pca 70000
+bsub -W 36:00 -n 8 -R "rusage[mem=6144]" -o pca_B5000_NAll python glo_pca.py --n_pca 70000
